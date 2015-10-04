fd50ea88951383e8a5d3b0d8f7a90b47a2fd4360
cjinfantino <cjinfantino@gmail.com>
2015-10-03 23:06:12 -0400

    add pythong template file

---
+++ tmpl/tmpl.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python2.7
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+__author__ = 'CJ Infantino'
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
